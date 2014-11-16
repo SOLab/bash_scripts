caef28ba73199fe9913d6ce2e3706d84aeb00b89
Alexander Myasoedov <alexander.myasoedov@gmail.com>
2014-11-16 21:56:53 +0300

    Modified ipnb servers to restart

--- ipnb_restart.sh
+++ ipnb_restart.sh
@@ -1,15 +1,15 @@
 killall ipython
 killall screen
-su - nonamenix -c 'screen ipython notebook --profile=nbserver'
+#~ su - nonamenix -c 'screen ipython notebook --profile=nbserver'
 su - monzik -c 'screen ipython notebook --profile=nbserver'
 su - ilya -c 'screen ipython notebook --profile=nbserver'
-su - karina -c 'screen ipython notebook --profile=nbserver'
-su - sergey -c 'screen ipython notebook --profile=nbserver'
-su - borisnp -c 'screen ipython notebook --profile=nbserver'
+#~ su - karina -c 'screen ipython notebook --profile=nbserver'
+#~ su - sergey -c 'screen ipython notebook --profile=nbserver'
+#~ su - borisnp -c 'screen ipython notebook --profile=nbserver'
 su - rinkorn -c 'screen ipython notebook --profile=nbserver'
-su - nansat -c 'screen ipython notebook --profile=nbserver'
+#~ su - nansat -c 'screen ipython notebook --profile=nbserver'
 #su - mag -c 'screen ipython notebook --profile=nbserver'
 
 su - soloveva -c 'screen ipython notebook --profile=nbserver'
 su - sdi -c 'screen ipython notebook --profile=nbserver'
-su - evgenym -c 'screen ipython notebook --profile=nbserver'
+#~ su - evgenym -c 'screen ipython notebook --profile=nbserver'
