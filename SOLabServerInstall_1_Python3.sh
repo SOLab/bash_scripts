a920d9dbc0a7dcf6c8bdbca99b171b7b7740f977
Alexander Myasoedov <alexander.myasoedov@gmail.com>
2016-12-01 09:27:39 +0300

    Installation scripts for Python3

---
+++ SOLabServerInstall_1_Python3.sh
@@ -0,0 +1,65 @@
+#~ Uninstall everything
+	LIST_OF_APPS="
+	ipython3
+	pyflakes
+	python3-docutils
+	python3-h5py
+	python3-matplotlib
+	python3-pyparsing
+	python3-pip
+	python3-psutil
+	python3-pyproj
+	python3-rope
+	python3-scipy
+	python3-setuptools 
+	python3-sphinx
+	python3-tornado
+	"
+sudo apt-get remove $LIST_OF_APPS
+
+#Installing the Scipy Stack
+# First install dev/dep files
+add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+apt-get update -qq
+apt-get install -y -qq python-pip
+
+
+	LIST_OF_APPS="
+	build-essential
+	cython3
+	gdal-bin
+	gfortran
+	git
+	git-core
+	libatlas-base-dev
+	libatlas-dev
+	libblas-dev
+	libfreetype6-dev
+	liblapack-dev
+	libgdal-dev
+	libgdal1-dev
+	libgeos-dev
+	libgeos-3.4.2
+	libgrib-api-dev
+	libgrib-api-tools
+	libgrib2c-dev
+	libhdf5-serial-dev
+	libjasper-dev
+	libnetcdf-dev
+	libpng12-dev
+	libproj-dev
+	psutils
+	python3-all-dev
+	python3-dev
+	python3-gdal
+	python3-psutil
+	python3-sip-dev
+	python3-pyqt4
+	subversion
+	"
+
+#~ python-mpltoolkits.basemap
+
+apt-get install -y -q --reinstall $LIST_OF_APPS
+
+# After this script run SOLabServerInstall_2.sh
---
+++ posada_install.sh
@@ -0,0 +1,19 @@
+mkdir -p /var/log/posada/
+touch /var/log/posada/posada.out && chmod 777 /var/log/posada/posada.out
+
+
+apt-get install git \
+				python3-pip \
+        		libblas-dev \
+                liblapack-dev \
+               	gfortran \
+                python3-gdal \
+                libgrib-api-dev \
+                libhdf5-dev \
+                libnetcdf-dev -y
+
+pip3 install --upgrade pip
+pip3 install numpy scipy pyresample shapely
+pip3 install --upgrade netcdf4
+pip3 install pygrib==2.0.1
+#pip install python-dateutil
